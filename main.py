eea1b206e73bc728af0b0044f55d0a3184e9fcd4
Andrea McIntosh <akmcinto@ualberta.ca>
2016-05-27 23:07:49 -0600

    Added colouring functions

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from PIL import Image
-import sys, os
+import sys, os, re
+from colourify import *
 
 def main(argv):
-    fname, ext = os.path.splitext(argv[0])
+    fpath, ext = os.path.splitext(argv[0])
+    fname = re.search('([\\/]\w+$)', fpath).group(0)
     img = Image.open(argv[0])
     currdir = os.path.dirname(os.path.realpath(__file__))
-    img.save(currdir + "\\testimg" + ext)
+
+    suffix = ""
+    for arg in argv[1:]:
+        s = ""
+        if arg == "greyify":
+            img, s = greyify(img)
+        if arg == "blueify":
+            img, s = blueify(img)
+        if arg == "redify":
+            img, s = redify(img)
+        if arg == "greenify":
+            img, s = greenify(img)
+        if arg == "yellowify":
+            img, s = yellowify(img)
+        if arg == "magentify":
+            img, s = magentify(img)
+        if arg == "cyanify":
+            img, s = cyanify(img)
+        suffix += "_" + s
+
+    img.save(currdir + fname + suffix + ext)
 
 if __name__ == "__main__":
     # http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html, 2016-05-26
