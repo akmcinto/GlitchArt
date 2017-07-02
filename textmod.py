709af172b1e1b460f18bea272ce68da749f9f56a
Andrea McIntosh <akmcinto@ualberta.ca>
2017-07-02 16:48:15 -0600

    Should work, but shuffleing mangles image too much

--- textmod.py
+++ textmod.py
@@ -6,26 +6,20 @@
 
 import re, random, array, io
 from PIL import Image
+from PIL import ImageDraw
 import numpy as np
+import binascii, io
 
 with open("IMAG2378_dered.jpg", "rb") as imageFile:
     f = imageFile.read()
     b = bytearray(f)
     m = re.split('\b', b)
-    # random.shuffle(m[-5:])
-    tmp = m[-5]
-    m[-5] = m[-2]
-    m[-2] = tmp
-    # p = np.asarray(m)
-    # p = array.fromlist('B', map(ord, m)).tostring()
+    random.shuffle(m[-5:])
     p = ''
     for x in m:
         p += x
-    img = Image.frombytes('RGB', (590, 640), str(p))
-    # img = Image.open(io.BytesIO(p))
-    img.save('tmp.jpg')
-
-
-    f2 = open('tmp.jpg', 'w')
-    f2.write(m)
-    f2.close()
+    stream = io.BytesIO(p)
+    img = Image.open(stream)
+    draw = ImageDraw.Draw(img)
+    img.save("a_test.jpg")
+    img.close()
